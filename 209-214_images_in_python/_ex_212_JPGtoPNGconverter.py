d261a0c397cfa5aaca1eaaceaa0abfdbe987c753
Wojciech Burman <wojbur94@gmail.com>
2022-10-01 15:00:51 +0200

    add solution file and clean up code

--- 209-214_images_in_python/_ex_212_JPGtoPNGconverter.py
+++ 209-214_images_in_python/_ex_212_JPGtoPNGconverter.py
@@ -1,5 +1,5 @@
 """
-Script to convert JPEG files into PNG. It accepts two command line arrguments:
+Simple script converting JPEG files into PNG. It accepts two command line arrguments:
 - First arg is a path to directory containing JPEG files
 - Second arg is a path to save converted PNG files to
 """
@@ -12,28 +12,28 @@ from PIL import Image
 
 # Grab the first and second argument
 try:
-    jpg_dir, png_dir = sys.argv[1], sys.argv[2]
-except IndexError:
-    print('Provide arguments: jpg directory and png direcory')
-    quit()
+    image_dir, output_dir = sys.argv[1], sys.argv[2]
+except IndexError as err:
+    raise Exception('Provide command line args: image dir and output dir') from err
 
 # Check if the first argument points to existing directory
-if not os.path.exists(jpg_dir):
-    print('First argument must point to existing directory')
-    quit()
+if not os.path.exists(image_dir):
+    raise Exception('First arg must point to existing directory')
 
-# Check if new folder exist, if not create it
-if not os.path.exists(png_dir):
-    os.mkdir(png_dir)
+# Check if output folder exist, if not create it
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
 
-# Loop through Pokedex
-for filename in os.listdir(jpg_dir):
-    file = os.path.join(jpg_dir, filename)
-    # check if it is a jpeg file
+# Loop through image folder
+for filename in os.listdir(image_dir):
+    file = os.path.join(image_dir, filename)
+
+    # Check if file is a jpeg
     if os.path.isfile(file) and os.path.splitext(
             file)[-1].lower() in ['.jpg', '.jpeg']:
-        img = Image.open(file)
-        # convert file to png and save
-        png_filename = Path(file).stem + '.png'
-        save_dir = os.path.join(png_dir, png_filename)
+        img = Image.open(os.path.join(image_dir, filename))
+        
+        # Convert file to png and save
+        output_filename = Path(file).stem + '.png'
+        save_dir = os.path.join(output_dir, output_filename)
         img.save(save_dir, 'png')
