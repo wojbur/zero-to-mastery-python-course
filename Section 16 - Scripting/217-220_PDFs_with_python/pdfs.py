fe43c0f4f8a2181a29f4673ee68083fa6de29f9e
Wojciech Burman <wojbur94@gmail.com>
2022-10-05 15:46:45 +0200

    lectures 217-220

---
+++ Section 16 - Scripting/217-220_PDFs_with_python/pdfs.py
@@ -0,0 +1,13 @@
+import PyPDF2
+
+
+with open ('dummy.pdf', 'rb') as file:
+    reader = PyPDF2.PdfFileReader(file)
+    # print(reader.getPage())
+    page = reader.getPage(0)
+    page.rotateCounterClockwise(90)
+    writer = PyPDF2.PdfFileWriter()
+    writer.addPage(page)
+    with open('tilt.pdf', 'wb') as new_file:
+        writer.write(new_file)
+        
---
+++ Section 16 - Scripting/221-224_Emails_with_python/email_sender.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path 
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Wojciech Burman'
+email['to'] = 'dummy@gmail.com'
+email['subject'] = 'You won 1,000,000 dollars!'
+
+email.set_content(html.substitute({'name': 'TinTin'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.login('throwaway@gmail.com', '321654987')
+  smtp.send_message(email)
+  print('all good boss!')
