55d50b23d67c884b1ea1919b165202c328b9ea24
Wojciech Burman <wojbur94@gmail.com>
2022-10-30 18:46:37 +0100

    section 19 flask basics

--- Section 19 - Web Development/web_server/server.py
+++ Section 19 - Web Development/web_server/server.py
@@ -1,8 +1,18 @@
-from flask import Flask
+import mimetypes
+import os
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Test"
+@app.route("/<username>/<int:post_id>")
+def main(username=None, post_id=None):
+    return render_template("index.html", name=username, post_id=post_id)
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/blog/2020/dogs")
+def blog2():
+    return "this is my dog"
