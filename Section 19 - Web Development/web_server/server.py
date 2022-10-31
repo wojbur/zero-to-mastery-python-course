61d27d278d4eaeddf8a222f2dbfa25d2f6f68569
Wojciech Burman <wojbur94@gmail.com>
2022-10-31 14:21:22 +0100

    add forms.html

--- Section 19 - Web Development/web_server/server.py
+++ Section 19 - Web Development/web_server/server.py
@@ -1,18 +1,23 @@
-import mimetypes
-import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route("/<username>/<int:post_id>")
-def main(username=None, post_id=None):
-    return render_template("index.html", name=username, post_id=post_id)
-
-@app.route("/about.html")
+@app.route("/forms.html")
 def about():
-    return render_template("about.html")
+    return render_template("forms.html")
 
-@app.route("/blog/2020/dogs")
-def blog2():
-    return "this is my dog"
+# @app.route("/<username>/<int:post_id>")
+# def main(username=None, post_id=None):
+#     return render_template("index.html", name=username, post_id=post_id)
+
+# @app.route("/about.html")
+# def about():
+#     return render_template("about.html")
+
+# @app.route("/blog/2020/dogs")
+# def blog2():
+#     return "this is my dog"
